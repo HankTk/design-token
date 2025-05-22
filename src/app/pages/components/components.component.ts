15294ef511c5f6f195e29a5a60094ba26c54bf06
HankTk <hidenori@comcast.net>
2025-05-22 15:57:20 -0700

    Merge buttons page into components page

--- src/app/pages/components/components.component.ts
+++ src/app/pages/components/components.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AxButtonComponent } from '../../components/ax-button/ax-button.component';
+import { AxBoxComponent } from '../../components/ax-box/ax-box.component';
 
 @Component({
   selector: 'app-components',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AxButtonComponent, AxBoxComponent],
   templateUrl: 'components.component.html',
   styleUrls: ['components.component.scss']
 })
